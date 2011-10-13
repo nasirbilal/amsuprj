8222782757935c829b77fcc703e67217c7c19609
tempsvnacc <tempsvnacc@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-10-13 14:13:41 +0000

    58/47

--- SLAM/src/slam/Main.java
+++ SLAM/src/slam/Main.java
@@ -14,6 +14,7 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       //Tämä on testicommit :---D
+        System.out.println("Heissan Laurin maailma.");
     }
 }
