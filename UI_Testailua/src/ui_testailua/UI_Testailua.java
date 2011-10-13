7722408e639c1400ea1c759414a728348016223b
tempsvnacc <tempsvnacc@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-10-13 13:37:33 +0000

    SVN-testi

---
+++ UI_Testailua/src/ui_testailua/UI_Testailua.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ui_testailua;
+
+/**
+ *
+ * @author Mudi
+ */
+public class UI_Testailua {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
