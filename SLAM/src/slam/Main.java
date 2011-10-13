f19afa3185dc11a62785b861a6c967edb30133db
tempsvnacc <tempsvnacc@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-10-13 14:16:14 +0000

    :D

--- SLAM/src/slam/Main.java
+++ SLAM/src/slam/Main.java
@@ -15,6 +15,6 @@ public class Main {
      */
     public static void main(String[] args) {
        //Tämä on testicommit :---D
-        System.out.println("Heissan Laurin maailma.");
+        System.out.println("Heissan");
     }
 }
