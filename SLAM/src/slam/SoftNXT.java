6ec42a18fc9301d447938c612ba2b9abbfaffbfc
lauri.m.viitanen <lauri.m.viitanen@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-10-17 08:43:44 +0000

    Softarobotin yksikkötestien runko.

---
+++ SLAM/src/slam/SoftNXT.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slam;
+
+/**
+ *
+ * @author k8
+ */
+public class SoftNXT {
+    SoftNXT(/*sensorit?*/) {
+        
+    }
+    
+    void rotate(double angles) {} /*paluuarvo? */
+    void travel(double dist) {} /*paluuarvo? */
+    double[] detDistancesOrSomething() { return null; } /* API? */
+}
