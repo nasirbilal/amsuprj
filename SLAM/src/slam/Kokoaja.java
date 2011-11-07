88ecf527cc9526016c97ed3ad41aa29299e4c06d
lauri.m.viitanen@gmail.com <lauri.m.viitanen@gmail.com@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-11-07 12:15:31 +0000

    Luotiin robottien mittauksista kartan kokoava luokka.

---
+++ SLAM/src/slam/Kokoaja.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slam;
+
+/**
+ *
+ * @author k8
+ */
+public class Kokoaja {
+    
+}
