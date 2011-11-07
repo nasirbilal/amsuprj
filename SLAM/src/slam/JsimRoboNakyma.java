cdc7d349eff282d3518feca0b8d46753b85e82ce
lauri.m.viitanen@gmail.com <lauri.m.viitanen@gmail.com@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-11-07 10:06:24 +0000

    Siistittiin ja lisättiin kommentteja.

--- SLAM/src/slam/JsimRoboNakyma.java
+++ SLAM/src/slam/JsimRoboNakyma.java
@@ -8,13 +8,14 @@ import java.awt.geom.Line2D;
  * @author juho vainio
  */
 
-
-
 public class JsimRoboNakyma {
     
-    private Line2D.Float katsetaulu[];     //(Astenäkymäviiva) Line2D.Float-olioita, jotka esittävät
-                            //(simu)robotin jostain suunnasta ottamaa "näkymää".
-    
+    /**
+     * Line2D.Float-olioita, jotka esittävät
+     * (simu)robotin jostain suunnasta ottamaa "näkymää".
+     */
+    private Line2D.Float katsetaulu[];
+
     /**
      * @param paikka on robotin paikka mittaushetkellä.
      * @param  suunta on robotin suunta mittaushetkellä.
@@ -23,51 +24,42 @@ public class JsimRoboNakyma {
      */
     public JsimRoboNakyma(Point2D.Float paikka, float katsesuunta, int mittausmaara, int infraEtaisyys){
         
-        /*
-         * Tässä luodaan robotin näkyöviivoista robon suuntaan ja paikkaan
-         * perustuen taulukko, jota vertaillaan sitten kartan viivoihin.
-         * 
-         * 
-         * 
-         */
-        
-        katsetaulu = new Line2D.Float[mittausmaara]; //näköviivojen taulukko
-        float katsekulma = 180/(mittausmaara-1);     //kuinka suuri kulma jää katseviivojen väliin
-        katsesuunta = katsesuunta - 90;              //katse vasemmalle
-        
-        for (int i = 0; i < katsetaulu.length; i++){
-            
-            //näköviivan pään X = x + (sensorin kantama) * sin (katseen suunta radiaaneina)
+        // Luodaan robotin näkyöviivoista robon suuntaan ja paikkaan
+        // perustuen taulukko, jota vertaillaan sitten kartan viivoihin.
+
+        katsetaulu = new Line2D.Float[mittausmaara]; // Näköviivojen taulukko
+        float katsekulma = 180 / (mittausmaara - 1); // Kuinka suuri kulma jää katseviivojen väliin.
+        katsesuunta = katsesuunta - 90;              // Katse vasemmalle
+
+        for (int i = 0; i < katsetaulu.length; i++){            
+            // Näköviivan pään X = x + (sensorin kantama) * sin (katseen suunta radiaaneina)
             float x = (float)(paikka.x + infraEtaisyys*Math.sin((katsesuunta*(Math.PI/180))));
             float y = (float)(paikka.y + infraEtaisyys*Math.cos((katsesuunta*(Math.PI/180))));
-            Point2D.Float aari = new Point2D.Float(x,y);
-            
-            katsetaulu[i] = new Line2D.Float(paikka, aari);
-            //näköviiva menee robotin nykyisestä paikasta
-            //laskettuun pisteeseen
-            
-            katsesuunta = katsesuunta + katsekulma; //seuraavan katseen suunta
+
+            // Näköviiva menee robotin nykyisestä paikasta laskettuun pisteeseen.
+            katsetaulu[i] = new Line2D.Float(paikka, new Point2D.Float(x,y));
+
+            katsesuunta += katsekulma; //seuraavan katseen suunta
         }
-        
     }
-    
+
     public Line2D.Float getNakoviiva(int i){
         return katsetaulu[i];
     }
+
     public Line2D.Float[] getNakotaulu(){
         return katsetaulu;
     }
-    
-    
+
+    /**
+     * Tarkista leikkaavatko viivat.
+     * 
+     * @param nakoviiva Robotin katseen viiva.
+     * @param karttaviiva Ympäristössä olevan esteen viiva.
+     * @return Point2D.Float, joka kertoo viivojen leikkauspisteen tai
+     *         null, jos viivat eivät leikkaa.
+     */
     public Point2D.Float leikkaako(Line2D.Float nakoviiva, Line2D.Float karttaviiva){
-        
-        /*
-         * Lejossin leikkikalukoodia
-         * parametreinä robotin näköviiva ja
-         * viiva kartasta. palauttaa ehkä
-         * leikkauspisteen, ehkä jotain muuta.
-         */
-        
         float x, y, a1, a2, b1, b2;
     
         if (nakoviiva.y2 == nakoviiva.y1 && karttaviiva.y2 == karttaviiva.y1) return null; // horizontal parallel
