679ea4690180ce384b245de839c5946cbdbcae38
lauri.m.viitanen@gmail.com <lauri.m.viitanen@gmail.com@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-11-09 11:28:38 +0000

    Luotiin robotin tietokoneelle kapseloiva luokka.

---
+++ SLAM/src/slam/RoboOhjain.java
@@ -0,0 +1,20 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package slam;
+
+/**
+ * @brief Luokka ohjaa robottia ja käsittelee siltä tulevaa dataa.
+ * 
+ * Bluetooth-yhteyden avulla luokka vastaanottaa robotilta sen sijaintitiedot
+ * ja kerätyt mittaustulokset. Luokka vastaa mittaustulosten yhdistämisestä
+ * robotin henkilökohtaisesti kartoittamaksi kartaksi ja sen perusteella robotin
+ * laskennallisen sijainnin määrittämisestä. Kaikki kommunikointi robotin kanssa
+ * tapahtuu tämän luokan kautta.
+ * 
+ * @author L
+ */
+public class RoboOhjain {
+    
+}
