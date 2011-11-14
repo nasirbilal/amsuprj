d959aa4fdeadc26c99794f1cc24b8d6ffe2fa8f2
lauri.m.viitanen@gmail.com <lauri.m.viitanen@gmail.com@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-11-14 14:42:12 +0000

    Luotiin kokoajan runko.

--- SLAM/src/slam/Kokoaja.java
+++ SLAM/src/slam/Kokoaja.java
@@ -7,30 +7,55 @@ package slam;
 import java.awt.geom.Line2D;
 import java.awt.geom.Point2D;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.TreeMap;
 
 /**
- * Kokoaja yhdistää robottien mittaustulokset kokonaiseksi kartaksi.
+ * Kokoaja yhdistää robottien yksityiset kartat kokonaiseksi yhteiskartaksi.
  * 
  * @author L
  */
 public class Kokoaja {
-    private static List<Line2D.Float> map;
+    private static List<Line2D.Float> kartta;
+    private static Map<Integer, Line2D.Float[] > roboKartta;
+    private static boolean onMuuttunut;
+
+    private static final int TARKKUUSASTEET = 8;
 
     Kokoaja() {
-        map = new ArrayList<Line2D.Float>();
+        kartta = new ArrayList<Line2D.Float>();
+        roboKartta = new TreeMap<Integer, Line2D.Float[] >();
+        onMuuttunut = false;
     }
 
-    Line2D.Float[] liitaKarttaan(Point2D.Float sijainti, float suunta,
-                                 float[] mittaustulokset) {
-        // Pyöritä pisteitä kunnes ne ovat nollakulmassa.
-        // Siirrä pisteitä kunnes ne ovat origossa.
-        // Liitä pisteet karttaan.
-        // ota huomioon, että esim. ekat mittaustulokset tulevat kahdelta tai
-        // useammalta robotilta mukamas origosta ja nollakulmassa...
-        // Mutta eihän niissä ole mitään samaa. Sekin on riski, että liitetään
-        // kaksi väärää palaa toisiinsa, koska niissä on yhtä pitkä suora seinä tai jotain...
-        // ehkä pitäisi kartat eritellä roboteittain ja sitten yhdistellä niitä?
-        return null;
+    /// @brief Tallettaa robotin tutkiman kartan kokoajan muistiin.
+    void asetaKartta(int id, Line2D.Float[] kartta) {
+        roboKartta.put(id, kartta);
+        onMuuttunut = true;
+    }
+
+    /// @brief Yhdistää aiemmin annetut robottien näkymät yhdeksi kartaksi.
+    Line2D.Float[] yhdista() {
+        
+        if (!onMuuttunut || roboKartta.isEmpty())
+            return kartta.toArray(new Line2D.Float[kartta.size()]);
+        
+        kartta.clear();
+        ArrayList<Line2D.Float[] > kartat = (ArrayList<Line2D.Float[] >) roboKartta.values();
+        kartta.addAll(Arrays.asList(kartat.get(0)));
+
+        for (int k = 1; k < kartat.size(); ++k) // kartat[0] lisättiin äsken.
+          for (int tark = TARKKUUSASTEET; tark >= 0 ; --tark )
+            for (int kulma = 0; kulma < 360; kulma += (1 + tark) * 10) {
+            
+            // Siirrä pisteitä kunnes ne ovat origossa.
+            // Liitä pisteet karttaan.
+        }
+
+        onMuuttunut = false;
+        return kartta.toArray(new Line2D.Float[kartta.size()]);
     }
 }
