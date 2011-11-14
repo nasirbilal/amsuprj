ba659e4f04d866ec205cd8fbe58f9cb1f2518924
lauri.m.viitanen@gmail.com <lauri.m.viitanen@gmail.com@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-11-14 11:15:51 +0000

    suunniteltiin ja dokumentoitiin rajapinta.

--- SLAM/src/slam/RoboOhjain.java
+++ SLAM/src/slam/RoboOhjain.java
@@ -4,6 +4,8 @@
  */
 package slam;
 
+import java.awt.geom.Point2D;
+
 /**
  * @brief Luokka ohjaa robottia ja käsittelee siltä tulevaa dataa.
  * 
@@ -14,28 +16,39 @@ package slam;
  * tapahtuu tämän luokan kautta. Jokaista robottia vastaa yksi RoboOhjain-
  * loukan ilmentymä.
  */
-public class RoboOhjain extends Thread {
+public class RoboOhjain {
 
-    private volatile boolean stop = false;
-    private BTYhteys bt = null;
-    private JsimData data;
-    private Mittaustulokset sailio;
-    private JsimRobo robo;
-
-    RoboOhjain(BTYhteys bt,JsimRobo robo,JsimData data, Mittaustulokset sailio) {
+    private int odotusMs; /// Kauanko BT-yhteyttä odotetaan ennen time outia.
+    private BTYhteys bt; /// BT-yhteys oikeaan tai simuloituun robottiin.
+   
+    RoboOhjain(BTYhteys bt, int odotusMilliSek) {
+        this.odotusMs = odotusMilliSek;
         this.bt = bt;
-        this.data = data;
-        this.sailio = sailio;
-        this.robo = robo;
     }
 
-    @Override
-    public void run(){
-        while (!stop) {
-            data = robo.valitseUusiPiste();
-            sailio.uusiMittaus(data);
-        }
+    /// @brief Palauttaa robotin mittaustulokset sijoitettuna koordinaatistoon.
+    ///
+    /// Robotin viimeksi mittaamat etäisyydet sijoitellaan mittauskulmien
+    /// perusteella karteesiseen koordinaatistoon niin, että robotin ollessa
+    /// origossa ja katsoesa positiivisen Y-akselin suuntaan havaitut esteet
+    /// sijaitsevat oikeassa suhteessa robottiin.
+    ///
+    /// @return Lista robotin suhteen sijoitelluista mittausetäisyyksistä. Jos
+    ///         estettä ei havaittu eli mittausetäisyys on ääretön, kyseinen
+    ///         koordinaatti on null.
+    public Point2D.Float[] haeEtaisyydet() {
+        return new Point2D.Float[37];
     }
 
-    public void lopeta() { stop = true; }
+    /// @brief Robotti siirretään uuteen sijaintiin ja suoritetaan mittaukset.
+    ///
+    /// Robotille lasketaan uusi mittauspiste ja mittaussuunta. Tiedot
+    /// lähetetään BT:n kautta robotille. Metodi odottaa, että robotti saa
+    /// mittaukset tehtyä ja vastaanottaa mittaustulokset BT:n kautta.
+    ///
+    /// @return True, jos kaikki meni hyvin ja false, jos mittaustuloksia ei
+    ///         saatu odotusajan umpeuduttua
+    public boolean teeMittaukset() {
+        return false;
+    }
 }
