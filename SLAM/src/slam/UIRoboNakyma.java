f312147dfd2a0073c5e5f86a29cfb33afe4e7f54
tanja.kantanen <tanja.kantanen@533cbb9b-180b-fb17-d264-965c79a0d302>
2011-11-23 11:17:51 +0000

    Korjattiin näkymän piirto skaalaavaksi. Huono valinta, mut parempi kuin ei mitään.

--- SLAM/src/slam/UIRoboNakyma.java
+++ SLAM/src/slam/UIRoboNakyma.java
@@ -38,39 +38,63 @@ public class UIRoboNakyma extends JPanel {
         super.paintComponent(g);
         g.drawRect(0, 0, getWidth() - 1, getHeight() - 1);
 
+        if (pisteet == null)
+            return;
+
+        // Laske sovituskertoimet saadulle datalle niin, että se mahtuu
+        // ikkunaan. Tässä pitäisi data skaalata jotenkin muutein kuin koko
+        // ikkunan kokoiseksi, esim. maksimietäisyyden mukaan, mutta tämä
+        // hoitakoon homman toistaiseksi.
+        double xmin = Float.MAX_VALUE;
+        double xmax = Float.MIN_VALUE;
+        double ymin = Float.MAX_VALUE;
+        double ymax = Float.MIN_VALUE;
+        for (Point2D.Double piste : pisteet)
+            if (piste != null) {
+            xmin = xmin > piste.x ? piste.x : xmin;
+            xmax = xmax < piste.x ? piste.x : xmax;
+            ymin = ymin > piste.y ? piste.y : ymin;
+            ymax = ymax < piste.y ? piste.y : ymax;
+        }
+        double xratio = getWidth()/(xmax - xmin);
+        double yratio = getHeight()/(ymax - ymin);
+
         //Piirretään viiva pisteiden välillä ja lopuksi vielä viivat
         // 'origosta' ensimmäiseen ja viimeiseen pisteeseen
         // tämä siis simuloi aluetta, jonka robotti näkee
-        if (pisteet != null) {
-            Point2D.Double edellinen = null, ensimmainen = null, viimeinen = null;
-            for (Point2D.Double piste : pisteet) {
-                if (ensimmainen == null) {
-                    ensimmainen = piste;
-                }
+        Point2D.Double edellinen = null, ensimmainen = null, viimeinen = null;
+        for (Point2D.Double piste : pisteet) {
+            if (piste == null)
+                continue;
 
-                if (edellinen != null) {
-                    g2.drawLine((int) edellinen.x + getWidth() / 2,
-                            (int) edellinen.y + getHeight(),
-                            (int) piste.x + getWidth() / 2,
-                            (int) piste.y + getHeight());
-                }
-
-                viimeinen = edellinen = piste;
+            if (ensimmainen == null) {
+                ensimmainen = piste;
             }
 
-            g2.setStroke(katkoV);
-            g2.setColor(Color.red);
+            if (edellinen != null) {
+                g2.drawLine((int) edellinen.x + getWidth() / 2,
+                        (int) edellinen.y + getHeight(),
+                        (int) piste.x + getWidth() / 2,
+                        (int) piste.y + getHeight());
+            }
 
-            //Pitää olla int,int,int,int ; ei int,int,double,double, tämä vain temp korjaus
+            viimeinen = edellinen = piste;
+        }
+
+        g2.setStroke(katkoV);
+        g2.setColor(Color.red);
+
+        //Pitää olla int,int,int,int ; ei int,int,double,double, tämä vain temp korjaus
+        if (ensimmainen != null)
             g2.drawLine(getWidth() / 2, getHeight(),
                     (int) ensimmainen.x + getWidth() / 2,
                     (int) ensimmainen.y + getHeight());
+        if (viimeinen != null)
             g2.drawLine(getWidth() / 2, getHeight(),
                     (int) viimeinen.x + getWidth() / 2,
                     (int) viimeinen.y + getHeight());
-            g2.setStroke(new BasicStroke(1));
-            g2.setColor(Color.black);
-        }
+        g2.setStroke(new BasicStroke(1));
+        g2.setColor(Color.black);
     }
 
     /**
